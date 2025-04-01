e7b240fae0a5ac6d34f83f58fc8a5ea0b04af252
Yunhao Qian <yunhaoq@seas.upenn.edu>
2025-04-01 03:57:19 -0400

    Fix the Lambert fragment shader

--- shaders/lambert.frag.glsl
+++ shaders/lambert.frag.glsl
@@ -8,8 +8,17 @@ out vec4 f_color;
 
 const vec3 lightDirection = normalize(vec3(0.5, 1.0, 0.75));
 
-void main() {
-    float diffuseTerm = dot(normalize(v_normal), lightDirection);
+void main()
+{
+    float diffuseTerm = max(dot(normalize(v_normal), lightDirection), 0.0);
+
+    bool xBound = fract(v_position.x) < 0.0125 || fract(v_position.x) > 0.9875;
+    bool yBound = fract(v_position.y) < 0.0125 || fract(v_position.y) > 0.9875;
+    bool zBound = fract(v_position.z) < 0.0125 || fract(v_position.z) > 0.9875;
+    if ((xBound && yBound) || (xBound && zBound) || (yBound && zBound)) {
+        diffuseTerm = 0.0;
+    }
+
     float ambientTerm = 0.2;
     float lightIntensity = diffuseTerm + ambientTerm;
     f_color = vec4(v_color * lightIntensity, 1.0);
