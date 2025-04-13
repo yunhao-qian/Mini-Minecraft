ced02ef180ceba8d2d83ba8d7c0520670d299682
Yunhao Qian <yunhaoq@seas.upenn.edu>
2025-04-12 20:53:23 -0400

    Address the shadow mapping limitation

--- shaders/shadow_depth.vert.glsl
+++ shaders/shadow_depth.vert.glsl
@@ -19,8 +19,10 @@ void main()
     vec4 viewSpacePosition = u_shadowViewMatrix * worldSpacePosition;
 
     v_shadowViewSpaceDepth = -viewSpacePosition.z;
-    // TODO: Calculating the screen-space position in this way causes obstacles outside of the
-    // camera's view frustum to not cast shadows, and the shadow effect to be dependent on the
-    // camera's orientation. This should be fixed.
     gl_Position = u_shadowProjectionMatrix * viewSpacePosition;
+
+    // Clamping the depth ensures that geometry outside the shadow frustum can still cast shadows,
+    // even if their actual depth values fall outside the valid clip space range. This may produce
+    // incorrect depth values, but the shadowing effect will still appear correctly.
+    gl_Position.z = clamp(gl_Position.z, -gl_Position.w, gl_Position.w);
 }
