6f011c0719847f6a53a756260edcbe55ed5bd49a
Yunhao Qian <yunhaoq@seas.upenn.edu>
2025-04-26 03:55:33 -0400

    Fix a numerical issue in the water wave function

--- resources/shaders/water_wave.glsl
+++ resources/shaders/water_wave.glsl
@@ -8,7 +8,8 @@ const vec2 WaterWaveAngularWaveVectors[WaterWaveCount] = vec2[](vec2(-0.08, -0.2
                                                                 vec2(0.23, 0.10));
 const float WaterWaveAngularFrequencies[WaterWaveCount] = float[](4.3, 3.2, 1.2, 4.2, 1.0, 2.4);
 const float WaterWavePhaseOffsets[WaterWaveCount] = float[](3.451, 4.134, 4.771, 4.065, 4.776, 5.552);
-const float WaterWaveExponents[WaterWaveCount] = float[](1.0, 2.0, 1.5, 2.5, 1.2, 1.6);
+// The exponent cannot be strictly 1.0, as it may cause NaN values when computing the derivatives.
+const float WaterWaveExponents[WaterWaveCount] = float[](1.1, 2.0, 1.5, 2.5, 1.2, 1.6);
 
 // Effective water simulation from physical models:
 // https://developer.nvidia.com/gpugems/gpugems/part-i-natural-effects/chapter-1-effective-water-simulation-physical-models
@@ -19,8 +20,8 @@ float getWaterWaveOffset(vec2 position, float time)
     for (int i = 0; i < WaterWaveCount; ++i) {
         float phase = dot(WaterWaveAngularWaveVectors[i], position) * WaterWaveAngularFrequencies[i]
                       + time * WaterWavePhaseOffsets[i];
-        offset += 2.0 * WaterWaveAmplitudes[i]
-                  * pow((sin(phase) + 1.0) * 0.5, WaterWaveExponents[i]);
+        float sinPositive = max((sin(phase) + 1.0) * 0.5, 0.0);
+        offset += 2.0 * WaterWaveAmplitudes[i] * pow(sinPositive, WaterWaveExponents[i]);
     }
     return offset;
 }
@@ -31,9 +32,10 @@ vec3 getWaterWaveNormal(vec2 position, float time)
     for (int i = 0; i < WaterWaveCount; ++i) {
         float phase = dot(WaterWaveAngularWaveVectors[i], position) * WaterWaveAngularFrequencies[i]
                       + time * WaterWavePhaseOffsets[i];
+        float sinPositive = max((sin(phase) + 1.0) * 0.5, 0.0);
         derivative += WaterWaveExponents[i] * WaterWaveAngularWaveVectors[i]
                       * WaterWaveAngularFrequencies[i] * WaterWaveAmplitudes[i]
-                      * pow((sin(phase) + 1.0) * 0.5, WaterWaveExponents[i] - 1.0) * cos(phase);
+                      * pow(sinPositive, WaterWaveExponents[i] - 1.0) * cos(phase);
     }
     return normalize(vec3(-derivative.x, 1.0, -derivative.y));
 }
